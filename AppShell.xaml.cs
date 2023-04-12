5250011a6c951c9f934c598ef6945e51959f9fb8
Andrew Stellman <astellman@stellman-greene.com>
2023-04-12 17:10:39 -0400

    Add project that shows <Border> issue

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BorderTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace BorderTest;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Flaming_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void Magic_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
